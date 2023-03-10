4a730c7793ff0b02096d98e25de4f064c4b91451
Alisher <alisherodilov482@gmail.com>
2023-03-10 05:14:05 -0800

    e!!!!!!!!!!!!!!!!!!!

--- src/main/java/com/example/rabbitmq/RabbitMqApplication.java
+++ src/main/java/com/example/rabbitmq/RabbitMqApplication.java
@@ -3,6 +3,7 @@ package com.example.rabbitmq;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @EnableRabbit
 @SpringBootApplication
 public class RabbitMqApplication {
